7a5b2f99f18db6fed35ba0460f0a8bfd16e8f13e
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-18 15:20:18 -0500

    update docker script

--- provisioners/docker.sh
+++ provisioners/docker.sh
@@ -2,8 +2,8 @@
 echo "Installing Docker and Docker Compose..."
 
 # Install Docker and Docker Compose
-sudo apt update
-sudo apt install -y docker.io docker-compose
+sudo apt-get update
+sudo apt-get install -y docker.io docker-compose
 
 # Enable and start Docker
 sudo systemctl enable docker
