c60ce744b781e9fef1ecc50b5ab2ec860563cb7b
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-18 10:38:58 -0500

    added code server and extensions installation

--- provisioners/vscode.sh
+++ provisioners/vscode.sh
@@ -3,6 +3,7 @@
 # Note this script assumes that dotfiles have been checked out to /tmp/dotfiles in a previous step
 
 echo "Installing Visual Studio Code..."
+DOTFILES_REPO="/tmp/dotfiles"
 
 # Make VSCode settings directory
 mkdir -p /home/vagrant/.config/Code/User
@@ -11,11 +12,37 @@ mkdir -p /home/vagrant/.config/Code/User
 chown -R vagrant:vagrant /home/vagrant/.config
 
 # Copy the VSCode settings from the dotfiles repository into the home directory
-cp /tmp/dotfiles/vscode/settings.json /home/vagrant/.config/Code/User/settings.json
+cp $DOTFILES_REPO/vscode/settings.json /home/vagrant/.config/Code/User/settings.json
 
 # Copy keybindings.json
-cp /tmp/dotfiles/vscode/keybindings.json /home/vagrant/.config/Code/User/keybindings.json
+cp $DOTFILES_REPO/vscode/keybindings.json /home/vagrant/.config/Code/User/keybindings.json
 
-# TODO install extensions
+# Install VSCode server
+echo "Installing Visual Studio Code Server..."
+curl -fsSL https://code.visualstudio.com/sha/download?build=stable &
+os=linux-x64 | tar -xz -C /home/vagrant/.vscode-server
+# Change ownership of the .vscode-server directory
+chown -R vagrant:vagrant /home/vagrant/.vscode-server
+echo "Visual Studio Code Server installed."
+
+# Install extensions
+if [ -f "$DOTFILES_REPO/vscode/extensions.txt" ]; then
+    echo "Installing VSCode extensions..."
+    # Locate the code binary
+    CODE_BIN=$(find /home/vagrant/.vscode-server/bin -name "code" -type f)
+
+    if [ -z "$CODE_BIN" ]; then
+        echo "VSCode Server binary not found!"
+        exit 1
+    fi
+
+    # Install extensions using the code binary
+    while IFS= read -r extension; do
+        echo "Installing VSCode extension $extension..."
+        $CODE_BIN --install-extension "$extension" --force
+    done <"$DOTFILES_REPO/vscode/extensions.txt"
+else
+    echo "No extensions list found in the dotfiles repository."
+fi
 
 echo "Visual Studio Code setup complete."
