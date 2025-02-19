91a0219b6b23f7d3df44e463b86ecda1d5f7df15
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-19 16:25:14 -0500

    updated UV install

--- provisioners/uv.sh
+++ provisioners/uv.sh
@@ -9,7 +9,7 @@ sudo apt-get update
 sudo apt-get install -y curl
 
 # Install uv as the vagrant user
-sudo -u vagrant curl -LsSf https://astral.sh/uv/install.sh | sh
+sudo -u vagrant curl -LsSf https://astral.sh/uv/install.sh | sudo -u vagrant sh
 
 # Add uv to the PATH
 echo "source /home/vagrant/.local/bin/env" | tee -a /home/vagrant/.zshrc
