ad6dc6cd14083a7c9c1ad8d68c53918dfc52336b
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-19 16:34:35 -0500

    updated pre-commit install

--- provisioners/pre-commit.sh
+++ provisioners/pre-commit.sh
@@ -3,7 +3,7 @@
 # Run this only after the UV installation
 echo "Installing pre-commit using UV..."
 
-# Install pre-commit
-uv pip install --system pre-commit
+# Install pre-commit as the vagrant user
+sudo -u vagrant uv pip install pre-commit
 
 echo "Pre-commit installation completed."
