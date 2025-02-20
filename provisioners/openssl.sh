b6edcb3129f47f9d833e8184981bd073f63b06c5
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-20 16:29:06 -0500

    updated openssl

--- provisioners/openssl.sh
+++ provisioners/openssl.sh
@@ -1,7 +1,8 @@
 #!/bin/zsh
 
 echo "Setting up OpenSSL configuration..."
-apt-get install pkg-config libssl-dev
+sudo apt-get update
+sudo apt-get install -y libssl-dev pkg-config
 # Add the following to the .zshrc
 echo 'export OPENSSL_DIR="/usr"' | tee -a /home/vagrant/.zshrc
 echo 'export OPENSSL_LIB_DIR="/usr/lib/aarch64-linux-gnu"' | tee -a /home/vagrant/.zshrc
