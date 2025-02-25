104baa209444f769ab757c1eb2c871dcd1c6c4be
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-24 20:02:31 -0500

    update docker compose installation

--- provisioners/docker.sh
+++ provisioners/docker.sh
@@ -3,7 +3,7 @@ echo "Installing Docker and Docker Compose..."
 
 # Install Docker and Docker Compose
 sudo apt-get update
-sudo apt-get install -y docker.io docker-compose
+sudo apt-get install -y docker.io
 
 # Enable and start Docker
 sudo systemctl enable docker
@@ -11,5 +11,10 @@ sudo systemctl start docker
 
 # Add vagrant user to the docker group
 sudo usermod -aG docker vagrant
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+# Apply executable permissions to the Docker Compose binary for all users
+sudo chmod +x /usr/local/bin/docker-compose
 
 echo "Docker installation completed."
