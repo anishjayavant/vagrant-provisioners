30dba43df36d654f1323191f63498fef017b8645
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-25 16:37:45 -0500

    added buildx setup

--- provisioners/docker.sh
+++ provisioners/docker.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 echo "Installing Docker and Docker Compose..."
+USER="vagrant"
 
 # Install Docker and Docker Compose
 sudo apt-get update
@@ -10,13 +11,18 @@ sudo systemctl enable docker
 sudo systemctl start docker
 
 # Add vagrant user to the docker group
-sudo usermod -aG docker vagrant
+sudo usermod -aG docker $USER
 
 # Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 # Apply executable permissions to the Docker Compose binary for all users
 sudo chmod +x /usr/bin/docker-compose
 # Enable BuildKit for Docker Compose by adding the following line to the .zshrc file for the vagrant user
-echo "export DOCKER_BUILDKIT=1" | tee -a /home/vagrant/.zshrc
+echo "export DOCKER_BUILDKIT=1" | tee -a /home/$USER/.zshrc
+
+# Install buildx for the vagrant user
+curl -fsSL https://github.com/docker/buildx/releases/download/v0.21.0/buildx-v0.21.0.linux-arm64 -o /home/$USER/.docker/cli-plugins/docker-buildx
+# Grant executable permissions to the buildx binary for the vagrant user
+chmod +x /home/$USER/.docker/cli-plugins/docker-buildx
 
 echo "Docker installation completed."
