e85b41735e3a751bf03e4f2420e8138b888a728b
Anish Jayavant <36860630+anishjayavant@users.noreply.github.com>
2025-02-26 15:43:56 -0500

    fixed unused shell var

--- provisioners/sqlx.sh
+++ provisioners/sqlx.sh
@@ -5,7 +5,6 @@ echo "Installing Rust sqlx CLI..."
 # Variables
 USER="vagrant"
 USER_HOME="/home/$USER"
-CARGO_ENV="$USER_HOME/.cargo/env"
 
 # Install sqlx CLI
 su - $USER -c "$USER_HOME/.cargo/bin/cargo install --version=0.8.3 sqlx-cli"
